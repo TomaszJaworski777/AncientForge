7a4fd5b433ee02205c45d45565aaedc2266dba9f
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2025-03-24 18:04:59 +0100

    Player script

---
+++ Assets/_Game/Scripts/Player/Player.cs
@@ -0,0 +1,12 @@
+﻿using AncientForge.Inventory;
+using UnityEngine;
+
+namespace AncientForge
+{
+	public class Player : MonoBehaviour
+	{
+		[SerializeField] private InventoryBase inventoryBase;
+
+		public InventoryBase Inventory => inventoryBase;
+	}
+}
